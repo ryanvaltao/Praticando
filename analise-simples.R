2bf96a9dda1685a1d168cd90fc724b81671b3189
ryanvaltao <rv_moura@id.uff.br>
2026-03-30 10:25:49 -0300

    Verificando estrutura de dados e analise descritiva - vars.qualitativas

--- analise-simples.R
+++ analise-simples.R
@@ -3,3 +3,16 @@
 # Leitura do banco de dados
 Dados = read.csv("dados - respostas formulário.csv", header=T, sep=";")
 attach(Dados)
+library(usethis)
+ 
+str(Dados)
+
+# Análises descritivas - variáveis qualitativas
+prop.table(table( ingresso))
+prop.table(table(sexo))
+prop.table(table(grupo_sanguineo))
+prop.table(table(ingere_pao))
+prop.table(table(ingere_massa))
+prop.table(table(ingere_refri))
+prop.table(table(ingere_alcool))
+prop.table(table(fruta_gosta))
