1cd3155803fd48cd8f709453b7e33f31c18f8c72
ryanvaltao <rv_moura@id.uff.br>
2026-03-30 10:13:43 -0300

    Adicionando banco de dados no repositório

---
+++ analise-simples.R
@@ -0,0 +1,5 @@
+# Script referente a aula prática de 30/03 de Git e GitHub, vinculado ao projeto Praticando
+
+# Leitura do banco de dados
+Dados = read.csv("dados - respostas formulário.csv", header=T, sep=";")
+attach(Dados)
