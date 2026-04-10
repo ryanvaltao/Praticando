3f9dabb7f7fe3aee117907ca43e0430e06f907de
ryanvaltao <rv_moura@id.uff.br>
2026-04-10 01:39:17 -0300

    Script esqueleto e dados

---
+++ analise-simples-com-branchs.R
@@ -0,0 +1,30 @@
+#script aula 7 praticando uso de branchs
+
+#-=-=-=leitura e manipulação de dados limpos=-=-=-
+dados = read.csv("dados-aula-7-limpo.csv", header=TRUE,sep=",")
+
+#-=-=-=vendo estrutura=-=-=-
+str(dados)
+head(dados)
+
+#-=-=-=calculo imc=-=-=-
+dados$imc = dados$peso / ((dados$altura/100)^2)
+View(dados)
+dados$imc = round(dados$imc, 0) #quis arredondar as casas decimais
+
+#-=-=-=faixa etaria=-=-=-
+dados$faixa_etaria = ifelse(dados$idade < 30, "jovem",
+                            ifelse(dados$idade < 60, "adulto","idoso"))
+dados$faixa_etaria = factor(dados$faixa_etaria)
+
+#-=-=-=analise descritiva com distribuição de frequencias e com graficos=-=-=-
+
+#preencher na branch analise graficos
+#usar comandos de distribuicao de frequencia de sexo, grafico de colunas para 
+#faixa etaria por sexo, hist de altura, box plot de imc por sexo
+
+#-=-=-=analise descritiva com medidas descritivas=-=-=-
+
+#preencher na branch analise medidas
+#usar comandos de calculo de minimo, maximo, media, mediana e
+#desvio padrao para idade, altura, peso e imc
